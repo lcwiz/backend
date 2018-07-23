fc8607206db722844b05bbf1888c7599f2e7b170
wizards23 <475359205@qq.com>
2018-07-23 22:36:48 +0800

    7月23日在家中提交代码。本次完成了websecurityconfig的配置，特别是passwordencoder的解决。

--- src/main/java/com/liuchang/backend/global/WebSecurityConfig.java
+++ src/main/java/com/liuchang/backend/global/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.liuchang.backend.global;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -10,6 +12,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .loginPage("/static/welcome.html")
                 .and()
                 .authorizeRequests()
+                .antMatchers("/static/welcome.html").permitAll()
                 .anyRequest()
                 .authenticated();
     }
